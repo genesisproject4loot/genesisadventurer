de02637dfde09f8a075bb0b131042497eb976aba
treppers <90061012+treppers@users.noreply.github.com>
2022-03-02 15:25:59 -0800

    updated contract

--- GenesisRenderer.sol
+++ GenesisRenderer.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 import "./Base64.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface IGenesisAdventurer {
     function getWeapon(uint256 tokenId) external view returns (string memory);
@@ -15,6 +16,7 @@ interface IGenesisAdventurer {
     function getOrderColor(uint256 tokenId) external view returns (string memory);
     function getOrderCount(uint256 tokenId) external view returns (string memory);
     function getLootTokenIds(uint256 tokenId) external pure returns(uint256[8] memory);
+    function getName(uint256 tokenId) external view returns (string memory);
 }
 
 interface ILootStats {
@@ -31,22 +33,35 @@ interface ILootStats {
     function getNumberOfItemsInClass(Class classType, uint256[8] memory tokenId) external view returns (uint256);
 }
 
-contract GenesisRenderer {
+contract GenesisRenderer is Ownable {
+    address public genesisAdventurerAddress;
+    address public lootStatsAddress;
+    address public genesisNamingAddress;
     IGenesisAdventurer private ga;
     ILootStats private stats;
 
     constructor(address genesisAdventurer_, address lootStats_) {
-        ga = IGenesisAdventurer(genesisAdventurer_);
-        stats = ILootStats(lootStats_);
+        setGenesisAdventurer(genesisAdventurer_);
+        setLootStats(lootStats_);
+    }
+
+    function setGenesisAdventurer(address addr_) public onlyOwner {
+        genesisAdventurerAddress = addr_;
+        ga = IGenesisAdventurer(addr_);
+    }
+
+    function setLootStats(address addr_) public onlyOwner {
+        lootStatsAddress = addr_;
+        stats = ILootStats(lootStatsAddress);
     }
 
     function tokenURI(uint256 tokenId) external view returns (string memory) {
 
-      string[23] memory parts;
-      string memory name = string(abi.encodePacked("Genesis Adventurer #", _toString(tokenId)));
+      string[27] memory parts;
+      string memory name = ga.getName(tokenId);
       uint256[8] memory lootTokenIds = ga.getLootTokenIds(tokenId);
       
-      parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; widht: 350px} .italic {font-style: italic}</style><rect width="100%" height="100%" fill="#000"/><rect y="300" width="350" height="50" fill="';
+      parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; widht: 350px} .italic {font-style: italic} .dark { fill: #000; fill-opacity: .5}</style><rect width="100%" height="100%" fill="#000"/><rect y="300" width="350" height="50" fill="';
       parts[1] = ga.getOrderColor(tokenId);
       parts[2] = '"/><text x="10" y="20" class="base">';
       parts[3] = ga.getWeapon(tokenId);
@@ -64,23 +79,28 @@ contract GenesisRenderer {
       parts[15] = ga.getNeck(tokenId);
       parts[16] = '</text><text x="10" y="160" class="base">';
       parts[17] = ga.getRing(tokenId);
-      parts[18] = '</text><text x="10" y="330" class="base italic">Genesis Adventurer of ';
-      parts[19] = ga.getOrder(tokenId);
-      parts[20] = ' ';
-      parts[21] = ga.getOrderCount(tokenId);
-      parts[22] = '</text></svg>';
+      parts[18] = '</text><text x="10" y="320" class="base italic">';
+      parts[19] = name;
+      parts[20] = '</text><text x="340" y="340" class="base dark" text-anchor="end">';
+      parts[21] = ga.getOrder(tokenId);
+      parts[22] = ' ';
+      parts[23] = ga.getOrderCount(tokenId);
+      parts[24] = '</text><text x="10" y="340" class="base dark">Rating ';
+      parts[25] = _toString(stats.getRating(lootTokenIds));
+      parts[26] = ' / 720</text></svg>';
 
       string memory image = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4]));
       image = string(abi.encodePacked(image, parts[5], parts[6], parts[7], parts[8], parts[9], parts[10]));
       image = string(abi.encodePacked(image, parts[11], parts[12], parts[13], parts[14], parts[15], parts[16]));
       image = string(abi.encodePacked(image, parts[17], parts[18], parts[19], parts[20], parts[21], parts[22]));
+      image = string(abi.encodePacked(image, parts[23], parts[24], parts[25], parts[26]));
       string memory attributes = string(abi.encodePacked('{"trait_type": "Order", "value": "', ga.getOrder(tokenId),'"},'));
-      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Greatness", "value": "', _toString(stats.getGreatness(lootTokenIds)),'"},'));
-      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Level", "value": "', _toString(stats.getLevel(lootTokenIds)),'"},'));
-      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Rating", "value": "', _toString(stats.getRating(lootTokenIds)),'"},'));
-      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Number of Warrior Items", "value": "', _toString(stats.getNumberOfItemsInClass(ILootStats.Class.Warrior, lootTokenIds)),'"},'));
-      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Number of Hunter Items", "value": "', _toString(stats.getNumberOfItemsInClass(ILootStats.Class.Hunter, lootTokenIds)),'"},'));
-      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Number of Mage Items", "value": "', _toString(stats.getNumberOfItemsInClass(ILootStats.Class.Mage, lootTokenIds)),'"},'));
+      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Greatness", "value": ', _toString(stats.getGreatness(lootTokenIds)),'},'));
+      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Level", "value": ', _toString(stats.getLevel(lootTokenIds)),'},'));
+      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Rating", "value": ', _toString(stats.getRating(lootTokenIds)),'},'));
+      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Number of Warrior Items", "value": ', _toString(stats.getNumberOfItemsInClass(ILootStats.Class.Warrior, lootTokenIds)),'},'));
+      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Number of Hunter Items", "value": ', _toString(stats.getNumberOfItemsInClass(ILootStats.Class.Hunter, lootTokenIds)),'},'));
+      attributes = string(abi.encodePacked(attributes, '{"trait_type": "Number of Mage Items", "value": ', _toString(stats.getNumberOfItemsInClass(ILootStats.Class.Mage, lootTokenIds)),'},'));
       attributes = string(abi.encodePacked(attributes, '{"trait_type": "Generation", "value": "Genesis"}'));
       string memory json = string(abi.encodePacked('{"name": "', name, '", "description": "This item is a Genesis Adventurer used in Loot (for Adventurers)", '));
       json = string(abi.encodePacked(json, '"attributes": [', attributes,'], '));
